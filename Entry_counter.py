296ca3f5154d00211c7d8356bd75bbbfcae1a2fd
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-06-12 13:12:16 -0300

    .

--- Entry_counter.py
+++ Entry_counter.py
@@ -1,8 +1,12 @@
 from urllib.request import urlopen 
 import csv
+import time
+
+inicio = time.time()
 
 # Caminho para o arquivo CSV
-path_to_csv = "C:\\Users\\João Fernando Rangel\\Desktop\\Worm_WebScrapping\\Urls\\URLs.csv"
+path_to_csv = "C:\\Users\\João Fernando Rangel\\Desktop\\Worm_WebScrapping\\Worm_Translator\\Urls\\URLs.csv"
+
 # Lista para armazenar as URLs
 URLS = []
 # Abre o arquivo CSV
@@ -17,4 +21,17 @@ with open(path_to_csv, mode='r', encoding='utf-8') as csv_file:
             cell_value = row[0]  # O índice 0 corresponde à primeira célula (n, 1)
             URLS.append(cell_value)
 
-# Imprime a lista URLS para verificar
+# Caminho para o arquivo de saída
+output_path = "C:\\Users\\João Fernando Rangel\\Desktop\\Worm_WebScrapping\\Worm_Translator\\TXTS_HTMLS\\output.txt"
+
+# Abre o arquivo de saída
+with open(output_path, "w+", encoding="utf-8") as arquivo:
+    for counter in URLS:
+        page = urlopen(counter)
+        html_bytes = page.read()
+        html = html_bytes.decode("utf-8")
+        arquivo.write(html)
+        arquivo.write("\n\n")  # Adiciona uma quebra de linha entre cada HTML para clareza
+
+fim = time.time()
+print(f"Tempo total: {fim-inicio} segundos")
